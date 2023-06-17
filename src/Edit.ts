391ed38ae4b5f077ac461414e3f0a8eaf99bcbeb
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-06-17 13:51:56 +0900

    pwa

---
+++ src/Edit.ts
@@ -0,0 +1,53 @@
+/*ボタンを押したときに、nodedataにnameとvalueを代入する
+*/
+$(function(){
+    $(document).on("click", "#idx", function(event){    
+        var NodeData: { name: string, lim: string } = { name: '', lim: '' };
+        NodeData["name"]  = $(".name").val() as string;; //.nameクラスから参照
+        NodeData["lim"] = $(".lim").val() as string;;//.limクラスから参照
+        save_data(NodeData);//save_dataにNodeDataを渡す
+    });
+});
+/*書き込み用関数
+*/
+function save_data(arg: { name: string; lim: string; }){
+    //ajaxはjsonはつかさどるライブラリ
+    $.ajax( {
+        type: 'post', //サーバー（こっち側）にデータを送信
+        url: './save.php',
+        data: arg,
+        //try.catch
+        success: function(){
+        alert('データを保存しました。');
+        },
+        error:function(){
+        alert('データの保存に失敗しました。');
+        }
+    });
+}
+
+//読み取り用関数
+function load_json(){
+$.ajax({
+    url: 'data.json',
+    cache : false,
+    dataType : 'json',
+    /*
+    成功した場合の流れ
+    まずjsonファイルからnameタグとlimタグのvalueを抜き出す
+    */
+    success: function(data) {
+        if($.inArray("address",node[i])){
+            $(".name").val(node["name"]);
+            $(".lim").val(node["lim"]);
+            console.log(data);
+        }else{
+            alert("false");
+        }
+    },
+    //エラー時にリロードする
+    error: function( data ) {
+        location.reload()
+    }
+});
+}
